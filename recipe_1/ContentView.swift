fa500fe1881423098fda5cdf15ced927ccc59b08
zhangweijun <ahnqihyun@sina.cn>
2020-06-01 13:01:40 +0800

    Initial Commit

---
+++ recipe_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe_1
+//
+//  Created by zhangweijun on 2020/6/1.
+//  Copyright © 2020 zhangweijun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
